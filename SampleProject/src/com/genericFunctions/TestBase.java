57b305fab3fa25aaa4ef4490dee059818998a885
Amar Waghmare <amarwaghmare573@gmail.com>
2022-10-06 10:40:07 +0530

    LoginPageFeature
    Hi Sohel,
    I have completed LoginPageFeature please review it.

--- SampleProject/src/com/genericFunctions/TestBase.java
+++ SampleProject/src/com/genericFunctions/TestBase.java
@@ -4,7 +4,7 @@ public class TestBase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TestBAse");
+		System.out.println("TestBase");
 	}
 
 }
