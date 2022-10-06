90cbf7e17c197c0049e544e926d919a2b549ed7a
Amar Waghmare <amarwaghmare573@gmail.com>
2022-10-06 10:18:55 +0530

    Added all Genetic Code
    Hi all,
    Strat with your indi tasks

---
+++ SampleProject/src/com/genericFunctions/ExtentReport.java
@@ -0,0 +1,9 @@
+package com.genericFunctions;
+
+public class ExtentReport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ExtentReport");
+	}
+}
---
+++ SampleProject/src/com/genericFunctions/TestBase.java
@@ -0,0 +1,10 @@
+package com.genericFunctions;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TestBAse");
+	}
+
+}
